8a374c1573ef30a32e0918e741823b87c482fd44
Jeff Dyer <jeffdyer@acm.org>
2012-09-02 15:31:18 -0700

    update codemap.*

--- test/codemap.js
+++ test/codemap.js
@@ -1,12 +1,13 @@
 function init(e) { }
-
+function mouseMove(e) { }
+function mouseUp(e) { }
+function showStatus() { }
 
 var nodeMap = {}
 var fixtureMap = {}
 var edgeMap = {}
 var leafNodeCount = 0
-
-var rootId
+var fullTree
 
 var cluster2
 
@@ -17,10 +18,36 @@ var vis2
 
 d3.json(mapName+".js.l7.l11.l9", function(ast) {
     makeNodeMap(ast)
+    getRootNode(ast).hidden = false
     d3.json(mapName+".js.l7.l11.l9.l12", function(tree) {
-	showGraph2(getTree(tree))
-    })
+	fullTree = tree
+	showTree(getTree(fullTree))  
+  })
 })
+
+function mouseDown(e) {
+    var node = d3.select(e.target)
+    var id = node.attr("id")
+    nodeMap[id].hidden = !nodeMap[id].hidden
+    leafNodeCount = 0
+    showTree(getTree(fullTree))
+}
+
+function getRootNode(ast) {
+    if ($.isArray(ast)) {
+	$.each(ast, function(i, v) {
+	    if ($.isArray(v)) {
+		makeNodeMap(v)
+	    }
+	    if (v.class = "Program") {
+		return v
+	    }
+	})
+    }
+    if (ast.class = "Program") {
+	return ast
+    }
+}
 
 function makeNodeMap(ast) {
     if ($.isArray(ast)) {
@@ -30,6 +57,7 @@ function makeNodeMap(ast) {
 	    }
 	    if (v.id) {
 		nodeMap[v.id] = v
+		v.hidden = true
 	    }
 	    if (v.class == "Fixture") {
 		fixtureMap[v.id] = v
@@ -55,6 +83,7 @@ function makeNodeMap(ast) {
     }
     if (ast.id) {
 	nodeMap[ast.id] = ast
+	ast.hidden = true
     }
     if (ast.elts) {
 	makeNodeMap(ast.elts)
@@ -97,19 +126,21 @@ function getTree(node) {
     }
     else
     if (node.id && node.edges) {
-	if (node.edges.length === 0) {
+	if (node.edges.length === 0 || nodeMap[node.id].hidden) {
 	    leafNodeCount++
 	}
 
-	$.each(node.edges, function(i, v) {
-	    var t = getTree(v)
-	    if ($.isArray(t)) {
-		kids = kids.concat(t)
-	    }
-	    else {
-		kids.push(t)
-	    }
-	});
+	if (!nodeMap[node.id].hidden) {
+	    $.each(node.edges, function(i, v) {
+		var t = getTree(v)
+		if ($.isArray(t)) {
+		    kids = kids.concat(t)
+		}
+		else {
+		    kids.push(t)
+		}
+	    });
+	}
         return {name: node.id, label: node.class, children: kids}
     }
     else
@@ -131,8 +162,8 @@ function getTree(node) {
     }
 }
 
-function showGraph2(tree) {
-    var width = 2000
+function showTree(tree) {
+    var width = 1000
     var height = leafNodeCount * 15
     cluster2 = d3.layout.cluster()
 	.size([height, width - 250]);
@@ -143,32 +174,38 @@ function showGraph2(tree) {
 	.attr("width", width)
 	.attr("height", height)
 
+    if (vis2)
+	vis2.remove()
+
     vis2 = vis.append("svg")
 	.attr("width", width)
 	.attr("height", height)
 	.append("g")
 	.attr("transform", "translate(40, 0)");
 
-  var link = vis2.selectAll("path.link")
-      .data(cluster2.links(nodes))
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", diagonal);
+    var link = vis2.selectAll("path.link")
+	.data(cluster2.links(nodes))
+        .enter().append("path")
+	    .attr("class", "link")
+	    .attr("d", diagonal);
 
   var node = vis2.selectAll("g.node")
       .data(nodes)
     .enter().append("g")
       .attr("class", "node")
+      .attr("id", function(d) { return d.name })
       .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
 
   node.append("circle")
-      .attr("r", 2);
+	.attr("fill", function (d) { return isTerminal(d.name) ? "red" : "" })
+	.attr("r", 2)
 
   node.append("text")
       .attr("dx", function(d) { return (d.children && d.children.length) ? 0 : 5; })
       .attr("dy", function(d) { return (d.children && d.children.length) ? 8 : 2; })
       .attr("text-anchor", function(d) { return (d.children && d.children.length) ? "end" : "start"; })
-      .text(function(d) { 
+      .attr("id", function(d) { return d.name })
+      .text(function(d) {
 	  return (d.children && d.children.length) ? d.label : getLabel2(d.name)
       });
 
@@ -189,12 +226,25 @@ function getLabel2(id) {
     case "LiteralBoolean":
     case "LiteralInt":
 	return node.class + ": " + node.elts[0]
-    case "LiteralObject":
-	return "{}"
-    case "LiteralArray":
-	return "[]"
     default:
 	return node.class
+    }
+}
+
+function isTerminal(id) {
+    var node = nodeMap[id]
+    switch (node.class) {
+    case "Identifier":
+    case "LiteralString":
+    case "LiteralBoolean":
+    case "LiteralInt":
+    case "Name":
+	return true
+    case "Head":
+	if (node.elts[0].length===0) { return true } else { return false }
+	break
+    default:
+	return false
     }
 }
 
